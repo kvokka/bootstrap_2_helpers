f46c1ef2a285bb4821a46750673d62050dd9a151
Kvokka <root_p@mail.ru>
2016-10-31 02:15:04 +0000

    Flash message colorize bug fix

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,13 +1,13 @@
 module BootstrapFlashHelper
-  AVALIABLE_TYPES = %i(info success warning danger)
+  AVALIABLE_TYPES = %w(info success warning danger)
   def bootstrap_flash
    flash_messages = []
    flash.each do |type, message|
      # Skip Devise :timeout and :timedout flags
-     next if type == :timeout
-     next if type == :timedout
-     type = :error   if type == :danger
-     use_type = AVALIABLE_TYPES.include?(type) ? type : :info
+     type_to_s = type.to_s
+     next if type_to_s == 'timeout' || type_to_s == 'timedout'
+     type_to_s = 'error' if type_to_s == 'danger'
+     use_type = AVALIABLE_TYPES.include?(type_to_s) ? type_to_s : :info
      text = content_tag(:div, 
               content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
               message, :class => "alert fade in alert-#{use_type}")
